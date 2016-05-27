17959289907523e06a13cbb1dd5b018caca4f374
Antoine145 <michelantoine145@gmail.com>
2016-05-27 16:35:39 +0200

    feature: page d'accueil view + menu

--- src/Reviz/FrontBundle/Controller/FrontController.php
+++ src/Reviz/FrontBundle/Controller/FrontController.php
@@ -11,7 +11,58 @@ class FrontController extends Controller
        * @Route("/", name="home")
        */
       public function indexAction() {
+            $em = $this->getDoctrine()->getManager();
 
-            return $this->render('RevizFrontBundle:Front:accueil.html.twig');
+            $levels = $em->getRepository('RevizFrontBundle:Level')->findAll();
+
+            return $this->render('RevizFrontBundle:Front:home.html.twig', ['levels' => $levels]);
+      }
+
+
+      /**
+       * @Route("/level/{id}", name="level")
+       */
+      public function showLevelAction() {
+            $em = $this->getDoctrine()->getManager();
+
+            $levels = $em->getRepository('RevizFrontBundle:Level')->findAll();
+
+            return $this->render('RevizFrontBundle:Front:showlevel.html.twig', ['levels' => $levels]);
+      }
+
+
+      /**
+       * @Route("/questions", name="questions")
+       */
+      public function questionsAction() {
+            $em = $this->getDoctrine()->getManager();
+
+            $levels = $em->getRepository('RevizFrontBundle:Level')->findAll();
+
+            return $this->render('RevizFrontBundle:Front:faq.html.twig', ['levels' => $levels]);
+      }
+
+
+      /**
+       * @Route("/about", name="about")
+       */
+      public function aboutAction() {
+            $em = $this->getDoctrine()->getManager();
+
+            $levels = $em->getRepository('RevizFrontBundle:Level')->findAll();
+
+            return $this->render('RevizFrontBundle:Front:about.html.twig', ['levels' => $levels]);
+      }
+
+
+      /**
+       * @Route("/contacts", name="contacts")
+       */
+      public function contactsAction() {
+            $em = $this->getDoctrine()->getManager();
+
+            $levels = $em->getRepository('RevizFrontBundle:Level')->findAll();
+
+            return $this->render('RevizFrontBundle:Front:contacts.html.twig', ['levels' => $levels]);
       }
 }
