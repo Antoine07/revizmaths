756f79a9c61733add430cd4b0df2bcffa99c9656
SimonArruti <simonarruti.dev@gmail.com>
2016-06-03 16:15:42 +0200

    feature crud course: creation
    ajout du formulaire de création fonctionnel

--- src/Reviz/FrontBundle/Controller/Admin/CourseController.php
+++ src/Reviz/FrontBundle/Controller/Admin/CourseController.php
@@ -36,6 +36,8 @@ class CourseController extends Controller
 
         $courses = $em->getRepository('RevizFrontBundle:Course')->findAll();
 
+        dump($courses);
+
         return $this->render('RevizFrontBundle:Back:Course/index.html.twig', array(
             'courses' => $courses,
         ));
@@ -51,10 +53,95 @@ class CourseController extends Controller
     {
         $course = new Course();
         $form = $this->createForm('Reviz\FrontBundle\Form\CourseType', $course);
+
+        $em = $this->getDoctrine()->getManager();
+        $levels = $em->getRepository('RevizFrontBundle:Level')->findAll();
+        $modules = $em->getRepository('RevizFrontBundle:Module')->findAll();
+        $categories = $em->getRepository('RevizFrontBundle:Category')->findAll();
+
+        $levelsFields = [];
+        foreach ($levels as $level) $levelsFields[$level->getName()] = $level->getId();
+
+        $modulesFields = [];
+        foreach ($modules as $module) $modulesFields[$module->getName()] = $module->getId();
+
+        $categoriesFields = [];
+        foreach ($categories as $category) $categoriesFields[$category->getName()] = $category->getId();
+
+        $levelsForm = $this->createFormBuilder()
+            ->add('level', ChoiceType::class, array(
+                'choices' => $levelsFields,
+            ))
+            ->getForm();
+
+        $modulesForm = $this->createFormBuilder()
+            ->add('module', ChoiceType::class, array(
+                'choices' => $modulesFields,
+            ))
+            ->getForm();
+
+        $categoriesForm = $this->createFormBuilder()
+            ->add('category', ChoiceType::class, array(
+                'choices' => $categoriesFields,
+            ))
+            ->getForm();
+
         $form->handleRequest($request);
+        $levelsForm->handleRequest($request);
+        $modulesForm->handleRequest($request);
+        $categoriesForm->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
+            $course = $form->getData();
+            $level = $levelsForm->getData();
+            $module = $modulesForm->getData();
+            $category = $categoriesForm->getData();
+
+            if (isset($level['level']) && isset($module['module']) && isset($category['category'])) {
+
+                // reset relation table post_taxonomy because must be unique and the relation is many to many
+                foreach ($levels as $resetLevel) $course->removeTaxonomy($resetLevel);
+
+                $levelEntity = $em->getRepository('RevizFrontBundle:Level')->findById((int)$level['level']);
+                $course->addTaxonomy($levelEntity[0]);
+
+                $moduleEntity = $em->getRepository('RevizFrontBundle:Module')->findById((int)$module['module']);
+
+                // check if level is parent of module nothing is wrong back with session flash message
+                if ($level['level'] != $moduleEntity[0]->getParentId()) {
+
+                    $this->session->getFlashBag()->add('warning', sprintf(
+                        'this relation must be correct with parent id, check module %s if parent with the level %s',
+                        $moduleEntity[0]->getName(),
+                        $levelEntity[0]->getName()
+                    ));
+
+                    return $this->redirectToRoute('admin_course_edit', array('id' => $course->getId()));
+                }
+
+                $categoryEntity = $em->getRepository('RevizFrontBundle:Category')->findById((int)$category['category']);
+                // check if category is in its own category
+                if ($module['module'] != $categoryEntity[0]->getParentId()) {
+
+                    $this->session->getFlashBag()->add('warning', sprintf(
+                        'this relation must be correct with parent id, check category %s if parent with the module %s',
+                        $categoryEntity[0]->getName(),
+                        $moduleEntity[0]->getName()
+                    ));
+
+                    return $this->redirectToRoute('admin_course_edit', array('id' => $course->getId()));
+                }
+
+                // module
+                foreach ($modules as $resetModule) $course->removeTaxonomy($resetModule);
+                $course->addTaxonomy($moduleEntity[0]);
+
+                // category
+                foreach ($categories as $resetCategory) $course->removeTaxonomy($resetCategory);
+                $course->addTaxonomy($categoryEntity[0]);
+
+            }
+
             $em->persist($course);
             $em->flush();
 
@@ -63,7 +150,11 @@ class CourseController extends Controller
 
         return $this->render('RevizFrontBundle:Back:Course/new.html.twig', array(
             'course' => $course,
-            'form' => $form->createView(),
+            'new_form' => $form->createView(),
+            'levels' => $levels,
+            'level_form' => $levelsForm->createView(),
+            'module_form' => $modulesForm->createView(),
+            'category_form' => $categoriesForm->createView(),
         ));
     }
 
@@ -91,7 +182,6 @@ class CourseController extends Controller
      */
     public function editAction(Request $request, Course $course)
     {
-
         $deleteForm = $this->createDeleteForm($course);
         $editForm = $this->createForm('Reviz\FrontBundle\Form\CourseType', $course);
 
@@ -109,19 +199,19 @@ class CourseController extends Controller
         $categoriesFields = [];
         foreach ($categories as $category) $categoriesFields[$category->getName()] = $category->getId();
 
-        $levelsForm = $this->createFormBuilder($levels)
+        $levelsForm = $this->createFormBuilder()
             ->add('level', ChoiceType::class, array(
                 'choices' => $levelsFields,
             ))
             ->getForm();
 
-        $modulesForm = $this->createFormBuilder($modules)
+        $modulesForm = $this->createFormBuilder()
             ->add('module', ChoiceType::class, array(
                 'choices' => $modulesFields,
             ))
             ->getForm();
 
-        $categoriesForm = $this->createFormBuilder($categories)
+        $categoriesForm = $this->createFormBuilder()
             ->add('category', ChoiceType::class, array(
                 'choices' => $categoriesFields,
             ))
@@ -194,6 +284,7 @@ class CourseController extends Controller
             'course' => $course,
             'edit_form' => $editForm->createView(),
             'levels' => $levels,
+            'modules' => $modules,
             'level_form' => $levelsForm->createView(),
             'module_form' => $modulesForm->createView(),
             'category_form' => $categoriesForm->createView(),
