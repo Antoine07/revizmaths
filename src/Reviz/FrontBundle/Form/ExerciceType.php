8eb7ec705f7b30cd1bc0cbce6d891d8ae38ce8c9
SimonArruti <simonarruti.dev@gmail.com>
2016-06-03 16:26:24 +0200

    feature_crud_course: first commit

--- src/Reviz/FrontBundle/Form/ExerciceType.php
+++ src/Reviz/FrontBundle/Form/ExerciceType.php
@@ -1,39 +1,21 @@
 <?php
+
 namespace Reviz\FrontBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class ExerciceType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('title', TextType::class, ['data' => 'caca','required' => true])
-            ->add('content', TextareaType::class, ['data' => 'caca','required' => false])
-            ->add('status', ChoiceType::class, ['choices' => [
-                'publié' => 'published', 'dépublié' => 'unpublished'
-            ]])
-            ->add('createdAt', DateTimeType::class)
-            ->add('submit', SubmitType::class, [
-                'label' => 'Create',
-                'attr' => array('class' => 'btn btn-default pull-right')
-            ])
-            ->add('delete', SubmitType::class);
-    }
 
+    /**
+     * @param OptionsResolver $resolver
+     */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'Reviz\FrontBundle\Entity\Post'
+            'data_class' => 'Reviz\FrontBundle\Entity\Exercice'
         ));
-
     }
 }
