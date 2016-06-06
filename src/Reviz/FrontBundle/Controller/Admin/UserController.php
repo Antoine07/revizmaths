39d86b38637277ddcc1a31fa8427b54f4d12a5cc
Antoine07 <antoine.lucsko@wanadoo.fr>
2016-06-06 13:59:36 +0200

    Namespace manquant dans la méthode UserController
    Les namespaces Route et Method sont remis dans UserController

--- src/Reviz/FrontBundle/Controller/Admin/UserController.php
+++ src/Reviz/FrontBundle/Controller/Admin/UserController.php
@@ -6,7 +6,8 @@ use Reviz\FrontBundle\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 /**
  * User controller.
  *
